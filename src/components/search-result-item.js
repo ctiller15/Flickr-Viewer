67eb1a66b4ad27e51e40d6d7e17bf91473334c61
ctiller15 <christiller2015@gmail.com>
2017-09-07 18:16:35 -0400

    Add in hover transitions w/ javascript

--- src/components/search-result-item.js
+++ src/components/search-result-item.js
@@ -1,37 +1,79 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const divStyle = {
-	// background: 'red',
-	width: '100%',
-	height: '100px',
-	marginTop: '10px',
-	position: 'relative'
-};
 
-const imgStyle = {
-	maxWidth: '100%',
-	maxHeight: '100%',
-	width: 'auto',
-	height: 'auto',
-	position: 'absolute',
-	top: '0',
-	bottom: '0',
-	left: '0',
-	right: '0',
-	margin: 'auto'
-}
 
-const SearchResultItem = (props) => {
+class SearchResultItem extends Component {
+	constructor(props){
+		super(props);
 
-	let photo = `https://farm${props.farm}.staticflickr.com/${props.server}/${props.id}_${props.secret}.jpg`;
-	return(
-			<div 
-				onClick={() => props.onImageSelect(photo)}
-				onMouseOver={function(){ this.style.background = 'red'}}
-				style={divStyle}>
-				<img className="img-thumbnail" style={imgStyle} src={photo}/>
-			</div>
-		)
+		this.state = {
+			hovered: false
+		}
+	}
+
+	divStyle() {
+		return {
+			// background: 'red',
+			width: '100%',
+			height: '100px',
+			marginTop: '10px',
+			position: 'relative'
+		}
+	};
+
+	imgStyle() {
+		if (this.state.hovered) {
+			return {
+				maxWidth: '100%',
+				maxHeight: '100%',
+				width: 'auto',
+				height: 'auto',
+				position: 'absolute',
+				top: '0',
+				bottom: '0',
+				left: '0',
+				right: '0',
+				margin: 'auto',
+				cursor: 'pointer',
+				transform: 'scale(1.15)'
+			}
+		} else {
+			return {
+				maxWidth: '100%',
+				maxHeight: '100%',
+				width: 'auto',
+				height: 'auto',
+				position: 'absolute',
+				top: '0',
+				bottom: '0',
+				left: '0',
+				right: '0',
+				margin: 'auto',
+				filter: 'opacity(.6)'
+			}
+		}
+	}
+
+	onMouseOver() {
+		this.setState({hovered: true});
+	}
+
+	onMouseOut() {
+		this.setState({hovered: false});
+	}	
+
+	render() {
+		let photo = `https://farm${this.props.farm}.staticflickr.com/${this.props.server}/${this.props.id}_${this.props.secret}.jpg`;
+		return(
+				<div 
+					onMouseOver={() => this.onMouseOver()}
+					onMouseOut={() => this.onMouseOut()}
+					onClick={() => this.props.onImageSelect(photo)}
+					style={this.divStyle()}>
+					<img className="img-thumbnail" style={this.imgStyle()} src={photo}/>
+				</div>
+			);
+		}
 }
 
 export default SearchResultItem;
